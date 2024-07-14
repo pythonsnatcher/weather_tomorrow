7aa6baa0806f3ad18768911b6b0fcb1707f2ec9e
pythonsnatcher <pythonsnatcher@gmail.com>
2024-07-14 15:20:17 +0100

    Create Repository_Structure.ino

---
+++ Repository_Structure.ino
@@ -0,0 +1,22 @@
+project_folder/
+│
+├── app.py
+│
+├── templates/
+│   └── index.html
+│
+├── static/
+│   ├── images/
+│   │   ├── sunny_intervals_and_a_gentle_breeze.png
+│   │   ├── light_rain_showers.png
+│   │   ├── first_quarter_moon.png
+│   │   └── ...
+│   ├── styles.css
+│   └── ...
+│
+├── venv/
+│   └── ...
+│
+├── requirements.txt
+│
+└── credentials.json
